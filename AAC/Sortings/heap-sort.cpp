509ab5f5bbf7e1f64152d2b945cb186b50f25782
ALVF <arindhimar116@gmail.com>
2024-09-08 16:59:46 +0530

    Sorting ALgos

--- AAC/Sortings/heap-sort.cpp
+++ AAC/Sortings/heap-sort.cpp
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void maxHeapify(vector<int> &arr, int n,int i)
+void maxHeapify(vector<int> &arr, int n, int i)
 {
     int largest = i;
-    int left = 2*i;
-    int right = 2*i+1;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-    if (left < n && arr[left] < arr[largest]) {
+    if (left < n && arr[left] > arr[largest]) {
         largest = left;
     }
 
-    if (right < n && arr[right] < arr[largest]) {
+    if (right < n && arr[right] > arr[largest]) {
         largest = right;
     }
 
-
-    if(largest!=i){
-        swap(arr[largest],arr[i]);
-        maxHeapify(arr,n,largest);
+    if (largest != i) {
+        swap(arr[largest], arr[i]);
+        maxHeapify(arr, n, largest);
     }
-
 }
 
-void heapSort(vector<int> &arr,int n){
-    for(int i=n/2;i>=0;i--){
-        maxHeapify(arr,n,i);
+void heapSort(vector<int> &arr, int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        maxHeapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]); 
+        maxHeapify(arr, i, 0);
     }
 }
 
@@ -33,18 +37,14 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> v;
-    for (int i = 0; i < n; i++)
-    {
-        int val;
-        cin >> val;
-        v.push_back(val);
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
     }
 
     heapSort(v, n);
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cout << v[i] << " ";
     }
     return 0;
