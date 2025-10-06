57eb3eba6000386818a45654223b7d1ab428b0c2
ALVF <arindhimar116@gmail.com>
2025-10-06 22:44:00 +0530

    changes

--- Sem - 3/Crypto/diffie_hellman.py
+++ Sem - 3/Crypto/diffie_hellman.py
@@ -1,15 +1,15 @@
 n = int(input("Enter a prime number (n): "))
 g = int(input("Enter a primitive root (g): "))
 
-x = int(input("Alice's private key (x): "))
+x = int(input("A's private key (x): "))
 A = pow(g, x, n)   # A = g^x mod n
 
-y = int(input("Bob's private key (y): "))
+y = int(input("B's private key (y): "))
 B = pow(g, y, n)  
 
 print("\nPublicly exchanged values:")
-print("Alice sends A =", A)
-print("Bob sends B =", B)
+print("A sends A =", A)
+print("B sends B =", B)
 
 KA = pow(B, x, n)  # A's computes key
 KB = pow(A, y, n)
--- Sem - 3/SC/q2.py
+++ Sem - 3/SC/q2.py
@@ -1,35 +1,40 @@
 def menu():
-    print("1 - Max Min")
-    print("2 - Max Product")
-    print("3 - Exit")
-    
-    
+    print("\n1 - Max Min Composition")
+    print("2 - Max Product Composition")
+    print("3 - Exit\n")
 
 while True:
-    a=[]
-    b=[]
-    
-    n=int(input("Enter number of elements in fuzzy set A: "))
+    a = []
+    b = []
+
+    n = int(input("Enter number of elements in fuzzy set A: "))
     for i in range(n):
-        ele=float(input(f"Enter element {i+1} of A: "))
+        ele = float(input(f"Enter element {i+1} of A: "))
         a.append(ele)
-    
-    m=int(input("Enter number of elements in fuzzy set B: "))
+
+    m = int(input("Enter number of elements in fuzzy set B: "))
     for i in range(m):
-        ele=float(input(f"Enter element {i+1} of B: "))
+        ele = float(input(f"Enter element {i+1} of B: "))
         b.append(ele)
-        
+
     menu()
-    opt=int(input("Select option: "))
-    if opt in [1,2] and n!=m:
-        print("Error: Fuzzy sets A and B must be of the same length for this operation.\n")
-        continue
-    
-    if opt==1:
-        max_min=[max(min(a[i],b[i]) for i in range(n))]
-        print("Max Min is:",max_min)
-    elif opt==2:
-        max_product=[max(a[i]*b[i] for i in range(n))]
-        print("Max Product is:",max_product)
-    elif opt==3:
+    opt = int(input("Select option: "))
+
+    if opt == 1:
+        result = [[min(a[i], b[j]) for j in range(m)] for i in range(n)]
+        print("\nMax-Min Composition Matrix:")
+        for row in result:
+            print(row)
+
+    elif opt == 2:
+        result = [[a[i] * b[j] for j in range(m)] for i in range(n)]
+        print("\nMax-Product Composition Matrix:")
+        for row in result:
+            print(row)
+
+    elif opt == 3:
+        print("Exiting...")
         break
+
+    else:
+        print("Invalid option. Try again.\n")
